3bf2931c1ee2b1209970e1ffa09d60d8b2784ad3
Shekinah Lutumba <166663598+SLutumba@users.noreply.github.com>
2026-09-17 06:45:58 +0200

    feat: add ownership protected task deletion

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import os
+
 from flask import Flask
-from app.api.tasks import tasks
+from flask_jwt_extended import JWTManager
+
+from app.api.tasks import task_blueprint
+from app.api.users import user_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(tasks)
+app.register_blueprint(task_blueprint)
+app.register_blueprint(user_blueprint)
+
+app.config["JWT_SECRET_KEY"] = os.environ["JWT_SECRET_KEY"]
+
+jwt = JWTManager(app)
 
 @app.route("/")
 def main():
